6aa9520a162fc06840f173de1a3801898e2cf8b0
marc <marc>
2004-09-23 16:29:45 +0000

    initial revision

---
+++ boot-scripts/etc/iscsi-lib.sh
@@ -0,0 +1,43 @@
+#
+# $Id: iscsi-lib.sh,v 1.1 2004-09-23 16:30:01 marc Exp $
+#
+# @(#)$File$
+#
+# Copyright (c) 2001 ATIX GmbH.
+# Einsteinstrasse 10, 85716 Unterschleissheim, Germany
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of ATIX
+# GmbH. ("Confidential Information").  You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with ATIX.
+#
+# Library for the iscsi-interface
+
+parser="iscsi://([^:]+):([[:digit:]]+)/"
+
+function getISCSIServerFromParam {
+    echo $1 | awk '
+{ 
+   match($1, "'$parser'", iscsiparms);
+   print iscsiparms[1];
+}'
+}
+
+function getISCSIPortFromParam {
+    echo $1 | awk '
+{ 
+   match($1, "'$parser'", iscsiparms);
+   print iscsiparms[2];
+}'
+}
+
+function createCiscoISCSICfgString {
+    echo "DiscoveryAddress=$1:$2"
+}
+
+# $Log: iscsi-lib.sh,v $
+# Revision 1.1  2004-09-23 16:30:01  marc
+# initial revision
+#
---
+++ boot-scripts/linuxrc.part.iscsi.sh
@@ -0,0 +1,48 @@
+#
+# $Id: linuxrc.part.iscsi.sh,v 1.1 2004-09-23 16:29:45 marc Exp $
+#
+# @(#)$File$
+#
+# Copyright (c) 2001 ATIX GmbH.
+# Einsteinstrasse 10, 85716 Unterschleissheim, Germany
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of ATIX
+# GmbH. ("Confidential Information").  You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with ATIX.
+#
+# Kernelparameter for changing the bootprocess for the comoonics generic hardware detection alpha1
+# iscsi=... The url to the iscsi-server targets: iscsi://server[:port]/
+
+#
+# Includes
+# %%include /etc/iscsi-lib.sh
+
+source etc/iscsi-lib.sh
+source etc/boot-lib.sh
+
+stage=0
+part="ISCSI"
+[ -z "$iscsi" ] && return 0
+iscsi_server=$(getISCSIServerFromParam $iscsi)
+iscsi_port=$(getISCSIPortFromParam $iscsi)
+iscsi_cfgfile="/etc/iscsi.cfg"
+echo_local_debug "$stage $part: ISCSI-Params: "
+echo_local_debug "$stage ${part}.1 ISCSI-Server: $iscsi_server"
+echo_local_debug "$stage ${part}.1 ISCSI-Port: $iscsi_port"
+
+echo_local -n "$part $stage: Creating iscsi-cfgfile"
+exec_local $(createCiscoISCSICfgString > $iscsi_cfgfile)
+
+echo_local_debug  "$part $stage: ISCSI CFG-File: $iscsi_cfgfile"
+exec_local_debug  cat $iscsi_cfgfile
+
+echo_local -n "$part $stage: Starting iscsi-client..."
+exec_local /etc/init.d/iscsi start
+
+# $Log: linuxrc.part.iscsi.sh,v $
+# Revision 1.1  2004-09-23 16:29:45  marc
+# initial revision
+#
