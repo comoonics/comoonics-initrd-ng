daa78e5ca16b0f778beb37b9d1aa07e63309659b
marc <marc>
2004-09-26 10:10:25 +0000

    initial revision

---
+++ create-gfs-initrd-generic.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+#
+# $Id: create-gfs-initrd-generic.sh,v 1.1 2004-09-26 10:10:25 marc Exp $
+#
+# @(#)$File$
+#
+# Copyright (c) 2001 ATIX GmbH.
+# Einsteinstrasse 10, 85716 Unterschleissheim, Germany
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of ATIX
+# GmbH. ("Confidential Information").  You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with ATIX.
+#
+. `dirname $0`/create-gfs-initrd-lib.sh
+
+function usage() {
+  echo "$0 -d dep_filename [-s initrdsize] [-m mountpoint] [-r rpm-list-file] [-b build-date-file] initrdname [kernel-version]"
+}
+
+function getoptions() {
+    while getopts vhm:fd:s:r:b: option ; do
+	case "$option" in
+	    v) # version
+		echo "$0 Version $id$"
+		exit 0
+		;;
+	    h) # help
+		usage
+		exit 0
+		;;
+	    m) # tempmount
+		mountpoint="$OPTARG"
+		;;
+	    f) # force
+		force=1
+		;;
+	    d) # depfile
+		dep_filename="$OPTARG"
+		;;
+	    s) # size
+		size="$OPTARG"
+		;;
+	    r ) # rpm-list
+		rpm_list="$OPTARG"
+		;;
+	    b) # release file
+		build_file="$OPTARG"
+		;;
+	    *)
+		echo "Error wrong option."
+		exit 1
+		;;
+	esac
+    done
+    shift $(($OPTIND - 1))
+    initrdname=$(echo $1 | sed s/.gz$//)
+    if [ -n "$2" ]; then kernel=$2; fi
+}
+
+if [ -z "$1" ]; then
+  usage
+  exit
+fi
+
+cwd=$(pwd)
+force=0
+mountpoint="/mnt/loop"
+#size=32768
+size=65536
+kernel=$(uname -r)
+getoptions $*
+
+touch ./.building_initrd
+#if [ -e $netenv_file ]; then . $netenv_file; fi
+
+echo -n "Makeing initrd ..."
+make_initrd $initrdname $size || (echo "(FAILED)" && exit $?)
+echo "(OK)"
+
+echo -n "Mounting initrd ..."
+mount_initrd $initrdname $mountpoint || (echo "(FAILED)" && exit $?)
+echo "(OK)"
+
+# extracting rpms
+if [ -n "$rpm_list" ]; then
+  echo -n "Extracting rpms..."
+  extract_all_rpms $rpm_list $mountpoint || (echo "(FAILED)" && exit $?)
+  echo "(OK)"
+fi
+
+files=`get_all_files_dependent $dep_filename | sort -u`
+# echo $files
+
+echo -n "Copying files..."
+cd $mountpoint
+for file in $files; do
+  if [ -d $file ]; then 
+    copy_file $file/\* ${mountpoint}/
+  else
+    dirname=`dirname $file`
+    create_dir ${mountpoint}$dirname
+    copy_file $file ${mountpoint}$dirname
+  fi
+done
+echo "(OK)"
+
+# copying kernel modules
+echo -n "Copying kernelmodules ($kernel)..."
+if [ ! -d ${mountpoint}/lib/modules/$kernel ]; then mkdir -p ${mountpoint}/lib/modules/; fi
+cp -r /lib/modules/$kernel ${mountpoint}/lib/modules/$kernel || (echo "(FAILED)" && exit $?)
+echo "(OK)"
+
+# patching build file
+if [ -n "$build_file" ]; then
+    echo -n "Patching buildfile \"$build_file\"..."
+    (echo "Build Date: "$(date) >> ${mountpoint}/$build_file && echo "(OK)") || echo "(FAILED)"
+fi
+
+
+#for module in $FC_MODULE $FC_MODULES $GFS_MODULES; do
+#  dirname=`dirname ${MODULES_DIR}/${module}`
+#  create_dir ${mountpoint}$dirname
+#  copy_file ${MODULES_DIR}/${module} ${mountpoint}$dirname
+#done
+
+#echo "The following files reside on the image:"
+#find .
+
+cd $cwd
+echo -n "Cleaning up ..."
+(chown -R root:root $mountpoint && \
+ sleep 1 && \
+ umount_and_zip_initrd $mountpoint $initrdname $force && \
+ rm ./.building_initrd) || (echo "(FAILED)" && exit $?)
+echo "(OK)"
+ls -lk $initrdname.gz
---
+++ create-gfs-initrd-lib.sh
@@ -0,0 +1,138 @@
+#
+# $Id: create-gfs-initrd-lib.sh,v 1.1 2004-09-26 10:10:25 marc Exp $
+#
+# @(#)$File$
+#
+# Copyright (c) 2001 ATIX GmbH.
+# Einsteinstrasse 10, 85716 Unterschleissheim, Germany
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of ATIX
+# GmbH. ("Confidential Information").  You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with ATIX.
+#
+
+function create_dir() {
+  local dirname=$1
+  if [ ! -e $dirname ]; then 
+    # echo "creating dir $dirname"; 
+    mkdir -p $dirname; 
+  fi
+}
+
+# copy the file like a dump one.
+function copy_file() {
+  local filename=$1
+  local source=$2
+#  echo "copying $filename to ${source}..."
+  cp -a $filename ${source}
+}
+
+# extract rpm
+# param: $1 rpm-name
+#        $2 dest
+function extract_rpm() {
+	 local rpm=$1
+	 local dest=$2
+	 cd $dest
+	 rpm2cpio $1 | cpio -ivdum
+	 cd -
+}
+
+# extract all rpm in file
+# param: $1: file
+#        $2: root
+function extract_all_rpms() {
+	 local rpms=`cat $1`
+	 local root=$2
+	 for rpm in $rpms; do
+    if [ ${rpm:0:1} != '#' ]; then
+		  extract_rpm $rpm $root
+    fi
+  done
+}
+
+# 
+# checks if the file is executable.
+# If so it returns all dependent libraries with path as a stringlist.
+function get_dependent_files() {
+  local filename=`which $1 2>/dev/null`
+  if [ -z $filename ]; then
+    filename=$1
+  fi
+  # file is a symbolic link
+  if [ -L $filename ]; then
+    echo $filename
+    local newfile=`ls -l $filename | sed -e "s/.* -> //"`
+    if [ "${newfile:0:1}" != "/" ]; then
+       echo `dirname $filename`/$newfile
+    else
+       echo $newfile
+    fi
+  # file is executable and not directory
+  elif [ -x $filename -a ! -d $filename ]; then
+    echo $filename
+    ldd $filename > /dev/null 2>&1
+    if [ $? = 0 ]; then
+      local newfiles=`ldd $filename | sed -e "s/^.*=> \(.*\) (.*).*$/\1/"`
+      for newfile in $newfiles; do
+         echo $newfile
+         if [ -L $newfile ]; then
+           local _newfile=`ls -l $newfile | sed -e "s/.* -> //"`
+           if [ "${_newfile:0:1}" != "/" ]; then
+             echo `dirname $newfile`/$_newfile
+           else
+             echo $_newfile
+           fi
+         fi
+      done
+    fi
+  # give back the file
+  else
+    echo $filename
+  fi
+}
+
+#
+# Takes a filename as argument and pipes all files listed in this file to 
+# get_dependent_files.
+function get_all_files_dependent() {
+  local filename=$1
+
+  while read line; do
+    if [ ${line:0:1} != '#' ]; then
+	get_dependent_files $line
+    fi
+  done <$filename
+}
+
+#
+# Creates a new memory filesystem initrd with the given size
+function make_initrd() {
+  local filename=$1
+  local size=$2
+  dd if=/dev/zero of=$filename bs=1k count=$size > /dev/null 2>&1 && \
+  mkfs.ext2 -F -m 0 -i 2000 $filename > /dev/null 2>&1
+}
+
+#
+# Mounts the given unpacked filesystem to the given directory
+function mount_initrd() {
+  local filename=$1
+  local mountpoint=$2
+  mount -o loop -t ext2 $filename $mountpoint > /dev/null 2>&1
+}
+
+#
+# Unmounts the given loopback memory filesystem and zips it to the given file
+function umount_and_zip_initrd() {
+  local mountpoint=$1
+  local filename=$2
+  local force=$3
+  local opts=""
+  [ -n "$force" ] && [ $force -gt 0 ] && opts="-f"
+  (umount $mountpoint && \
+  gzip $opts -9 $filename) || (lsof | grep "$mountpoint")
+}
