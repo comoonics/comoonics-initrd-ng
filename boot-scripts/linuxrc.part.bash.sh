1ce90ffc5da0e72862cd04157da8a54f3731c099
marc <marc>
2006-01-28 15:12:55 +0000

    added cvs tags changed to new bootconcept

--- boot-scripts/linuxrc.part.bash.sh
+++ boot-scripts/linuxrc.part.bash.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# $Id: linuxrc.part.bash.sh,v 1.3 2006-01-28 15:12:55 marc Exp $
 
-exec /bin/bash > /dev/console 2>&1
+exit_linuxrc 0 "/bin/bash"
 
+######################
+# $Log: linuxrc.part.bash.sh,v $
+# Revision 1.3  2006-01-28 15:12:55  marc
+# added cvs tags
+# changed to new bootconcept
+#
