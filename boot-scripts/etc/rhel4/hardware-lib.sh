5dfa0d8efd9b737f0eeb0fb7fbc610e36622ba77
marc <marc>
2006-07-03 08:33:26 +0000

    changed hardwaredetection

--- boot-scripts/etc/rhel4/hardware-lib.sh
+++ boot-scripts/etc/rhel4/hardware-lib.sh
@@ -1,5 +1,5 @@
 #
-# $Id: hardware-lib.sh,v 1.2 2006-05-12 13:06:41 marc Exp $
+# $Id: hardware-lib.sh,v 1.3 2006-07-03 08:33:26 marc Exp $
 #
 # @(#)$File$
 #
@@ -35,16 +35,21 @@
 #  SOURCE
 #
 function rhel4_hardware_detect() {
+  cp ${modules_conf} ${modules_conf}.tmpl
   exec_local /usr/sbin/kudzu -t 30 -c SCSI -q 
   mv ${modules_conf} ${modules_conf}.scsi
+  cp ${modules_conf}.tmpl ${modules_conf}
   #exec_local /usr/sbin/kudzu -t 30 -c RAID -q 
   #mv ${modules_conf} ${modules_conf}.raid
   exec_local /usr/sbin/kudzu -t 30 -c USB -q 
   mv ${modules_conf} ${modules_conf}.usb
+  cp ${modules_conf}.tmpl ${modules_conf}
   exec_local /usr/sbin/kudzu -t 30 -c NETWORK -q
-  cat ${modules_conf}.scsi > ${modules_conf}
+  cat ${modules_conf}.scsi >> ${modules_conf}
   #	cat ${modules_conf}.raid >> ${modules_conf}
   cat ${modules_conf}.usb >> ${modules_conf}
+  cp ${modules_conf} ${modules_conf}.tmpl
+  cat ${modules_conf}.tmpl | sort -u > ${modules_conf}
 
   return $return_c
 }
@@ -52,7 +57,10 @@ function rhel4_hardware_detect() {
 
 #############
 # $Log: hardware-lib.sh,v $
-# Revision 1.2  2006-05-12 13:06:41  marc
+# Revision 1.3  2006-07-03 08:33:26  marc
+# changed hardwaredetection
+#
+# Revision 1.2  2006/05/12 13:06:41  marc
 # First stable Version 1.0 for initrd.
 #
 # Revision 1.1  2006/05/07 11:33:40  marc
