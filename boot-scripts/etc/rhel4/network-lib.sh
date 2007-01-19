db364bc9311d6e491b64c851e4dfe27f6c690fc5
mark <mark>
2007-01-19 10:04:16 +0000

    added vlan support

--- boot-scripts/etc/rhel4/network-lib.sh
+++ boot-scripts/etc/rhel4/network-lib.sh
@@ -1,5 +1,5 @@
 #
-# $Id: network-lib.sh,v 1.2 2006-05-12 13:03:24 marc Exp $
+# $Id: network-lib.sh,v 1.3 2007-01-19 10:04:16 mark Exp $
 #
 # @(#)$File$
 #
@@ -60,25 +60,30 @@ function rhel4_ip2Config() {
     mv ${__prefix}/etc/sysconfig/network-scripts/ifcfg-$ipDevice ${__prefix}/etc/sysconfig/network-scripts/ifcfg-${ipDevice}.com_back
   fi
   if [ -n "$ipAddr" ]; then
-    if [ "$ipAddr" = "dhcp" -o "$ipAddr" = "DHCP" -o -z "$ipAddr" ]; then 
+    if [ "$ipAddr" = "dhcp" -o "$ipAddr" = "DHCP" -o -z "$ipAddr" ]; then
       bootproto="dhcp"
-    else 
+    else
       bootproto="none"
     fi
-    (echo "DEVICE=$ipDevice" && 
-     echo "BOOTPROTO=$bootproto" && 
+
+    (echo "DEVICE=$ipDevice" &&
+     echo "BOOTPROTO=$bootproto" &&
      echo "ONBOOT=no" &&
      echo "TYPE=Ethernet") > ${__prefix}/etc/sysconfig/network-scripts/ifcfg-$ipDevice
     if [ "$bootproto" != "dhcp" ]; then
-      (echo "IPADDR=$ipAddr" && 
-       if [ -n "$ipNetmask" ]; then echo "NETMASK=$ipNetmask"; fi) >> ${__prefix}/etc/sysconfig/network-scripts/ifcfg-$ipDevice
-      if [ -n "$ipGate" ]; then 
-	echo "GATEWAY=$ipGate" >> ${__prefix}/etc/sysconfig/network-scripts/ifcfg-$ipDevice
+      (echo "IPADDR=$ipAddr" &&
+      if [ -n "$ipNetmask" ]; then echo "NETMASK=$ipNetmask"; fi) >> ${__prefix}/etc/sysconfig/network-scripts/ifcfg-$ipDevice
+      if [ -n "$ipGate" ]; then
+	    echo "GATEWAY=$ipGate" >> ${__prefix}/etc/sysconfig/network-scripts/ifcfg-$ipDevice
       fi
+      # test for vlan config
+	  if [[ "$ipDevice" =~ "[a-z]+[0-9]+\.[0-9]+" ]]; then
+		echo "VLAN=yes" >> ${__prefix}/etc/sysconfig/network-scripts/ifcfg-$ipDevice
+	  fi
     fi
   else
-    (echo "DEVICE=$ipDevice" && 
-     echo "BOOTPROTO=none" && 
+    (echo "DEVICE=$ipDevice" &&
+     echo "BOOTPROTO=none" &&
      echo "ONBOOT=no" &&
      echo "MASTER=${master}" &&
      echo "SLAVE=${slave}" &&
@@ -86,8 +91,8 @@ function rhel4_ip2Config() {
   fi
 #   (echo "NETWORKING=yes" &&
 #    echo "HOSTNAME=$ipHostname") > ${__prefix}/etc/sysconfig/network
-#   if [ $(/bin/hostname) = "(none)" ] || [ $(/bin/hostname) = "localhost.localdomain" ] || [ $(/bin/hostname) = "localhost" ]; then 
-#       /bin/hostname $ipHostname; 
+#   if [ $(/bin/hostname) = "(none)" ] || [ $(/bin/hostname) = "localhost.localdomain" ] || [ $(/bin/hostname) = "localhost" ]; then
+#       /bin/hostname $ipHostname;
 #   fi
 #   echo_local_debug "   /etc/sysconfig/network"
 #   exec_local_debug cat /etc/sysconfig/network
@@ -95,11 +100,14 @@ function rhel4_ip2Config() {
 #   exec_local_debug cat /etc/sysconfig/network-scripts/ifcfg-${ipDevice}
    return 0
 }
-#************ rhel4_ip2Config 
+#************ rhel4_ip2Config
 
 #################
 # $Log: network-lib.sh,v $
-# Revision 1.2  2006-05-12 13:03:24  marc
+# Revision 1.3  2007-01-19 10:04:16  mark
+# added vlan support
+#
+# Revision 1.2  2006/05/12 13:03:24  marc
 # First stable version 1.0.
 #
 # Revision 1.1  2006/05/07 11:33:40  marc
