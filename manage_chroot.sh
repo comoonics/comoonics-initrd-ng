2123138373aa1fde86294727eb83805689717fff
marc <marc>
2009-04-14 15:07:11 +0000

    - added xfiles support - moved patching of initscripts here

--- manage_chroot.sh
+++ manage_chroot.sh
@@ -7,7 +7,7 @@
 #  DESCRIPTION
 #*******
 #
-# $Id: manage_chroot.sh,v 1.12 2009-02-23 12:58:48 marc Exp $
+# $Id: manage_chroot.sh,v 1.13 2009-04-14 15:07:11 marc Exp $
 #
 # @(#)$File$
 #
@@ -39,6 +39,9 @@ exec 5>> $logfile
 exec 6>> $logfile
 exec 7>> $logfile
 
+if [ -f /etc/sysconfig/comoonics-chroot ]; then
+	source /etc/sysconfig/comoonics-chroot
+fi
 
 # include libraries
 
@@ -58,10 +61,11 @@ repository_store_value rootfs $rootfs
 sourceRootfsLibs $(dirname $0)/boot-scripts
 distribution=$(repository_get_value distribution)
 
-clusterfs_chroot_needed init
-__default=$?
-chrootneeded=$(getParameter chroot $__default)
-
+if [ -z "$chrootneeded" ]; then
+  clusterfs_chroot_needed init
+  __default=$?
+  chrootneeded=$(getParameter chroot $__default)
+fi
 initEnv
 
 # dep_filename is the file where all dependent files are to be taken from
@@ -104,9 +108,9 @@ EOF
 # TO DO move functions to boot-lib.sh
 function log() {
 	if [ "$verbose" ]; then
-		echo_local $*
+		echo_local $@
 	else 
-		echo_out $*
+		echo_out $@
 	fi 
 }
 
@@ -125,16 +129,37 @@ function umount_chroot() {
 }
 
 function mount_chroot() {
-	if ! mount | grep "$chrootdir/dev/pts" &> /dev/null; then
-		mount -t devpts none $chrootdir/dev/pts
+	local retc=0
+	local subdir fstype device
+	local i=
+	local mounts=( "/dev/pts" "devpts" "none"  "/proc" "proc" "proc" "/sys" "sysfs" "sysfs" )
+	if [ -z "$chrootdir" ]; then
+		return 1
 	fi
-	if ! mount | grep "$chrootdir/proc" &> /dev/null; then
-		mount -t proc proc $chrootdir/proc
-	fi
-	if ! mount | grep "$chrootdir/sys" &> /dev/null; then
-		mount -t sysfs none $chrootdir/sys
-	fi
-	
+	# We end up in this clause only if we need a chroot but there is none yet.
+	if [ -z "$chrootdir" ]; then
+  		echo_local -n "Building comoonics chroot environment \"$chrootdir\" "
+  		res=( $(build_chroot $(repository_get_value cluster_conf) $(repository_get_value nodename)) )
+  		retc=$?
+  		repository_store_value chroot_mount ${res[0]}
+  		#FIXME: chroot_path should be the same as chrootdir but what to do here? How to decide?
+  		repository_store_value chroot_path ${res[1]}
+		echo_local "res: $res -> chroot_mount="$(repository_get_value chroot_mount)", chroot_path="$(repository_get_value chroot_path)
+		return_code $retc 
+	fi		
+	echo_local -n "subdirs "
+	while [ ${#mounts} -gt 0 ]; do
+		subdir=${mounts[0]}
+		fstype=${mount[1]}
+		device=${mount[2]}
+		mounts=( ${mounts[@]:3} )
+		if ! is_mounted "${chrootdir}${subdir}" && ! is_mounted $(repository_get_value cdsl_local_dir)${chrootdir}${subdir}; then
+			echo_local -n "..${subdir}.."
+			mkdir -p $chrootdir${subdir} >/dev/null 2>&1
+			mount -t $fstype $device $chrootdir${subdir}
+		fi
+	done
+	true
 }
 
 function stop_service_pid() {
@@ -161,35 +186,35 @@ function status_service_cmd() {
 }
 
 function update_chroot() {
-	log "starting update_chroot"
+	#log -n "starting update_chroot "
+	local rc=0
 
 	if [ ! -e $dep_filename ]; then
-		log "dependency file $dep_filename does not exist"
+		echo_local "dependency file $dep_filename does not exist"
 		exit 1
 	fi
 
 	if [ ! -e $rpm_filename ]; then
-		log "rpm_filename $rpm_filename does not exist!"
+		echo_local "rpm_filename $rpm_filename does not exist!"
 	fi
 
 	# extracting rpms
 	if [ -n "$rpm_filename" ] && [ -e "$rpm_filename" ]; then
+		echo_local -n "rpmfiles.."
   		extract_all_rpms $rpm_filename $chrootdir $rpm_dir $verbose
-  		rc=$?
-  		log "Extracted rpms: $(return_code $rc)" 
+		[ $rc -eq 0 ] && rc=$?
 	fi
 
-	log "Retreiving dependent files"
+	echo_local -n "dependent files "
 
 	files=( $(get_all_files_dependent $dep_filename $verbose | sort -u | grep -v "^.$" | grep -v "^..$"| tr '&' '\n'))
-	rc=$?
+	[ $rc -eq 0 ] && rc=$?
+	echo_local -n "\"${#files[@]}\" .." 
 
-	log "found ${#files[@]} files $(return_code $rc) "
-
-	log "Copying files..."
+	echo_local -n "copying... "
 	copy_filelist $chrootdir ${files[@]}
-
-	log "copy files: $(return_code $rc)"
+	[ $rc -eq 0 ] && rc=$?
+	return $rc
 }
 
 function mount_cdsl {
@@ -208,6 +233,99 @@ function mount_cdsl {
 	fi
 	clusterfs_mount_cdsl $newroot $cdsl_local $nodeid $cdsl_path	
 }
+
+function createxfiles {
+	repository_store_value xtabfile /etc/xtab
+	repository_store_value xrootfsfile /etc/xrootfs
+	repository_store_value xkillallprocsfile /etc/xkillallprocs_file
+	clusterfs_chroot_needed initrd
+	__default=$?
+	getParameter chrootneeded $__default &>/dev/null
+	echo_local -n "Writing xtab.. "
+	if [ $(repository_get_value chrootneeded) -eq 0 ]; then
+  		create_xtab "$(repository_get_value xtabfile)" "$(repository_get_value cdsl_local_dir)" "$(repository_get_value chroot_mount)" 
+	else  
+  		create_xtab "$(repository_get_value xtabfile)" "$(repository_get_value cdsl_local_dir)"
+	fi
+	success
+
+	echo_local -n "Writing xrootfs.. "
+	create_xrootfs $(repository_get_value newroot)/$(repository_get_value xrootfsfile) $(repository_get_value rootfs)
+	success
+
+	echo_local -n "Writing xkillall_procs.. "
+	create_xkillall_procs "$(repository_get_value newroot)/$(repository_get_value xkillallprocsfile)" "$(repository_get_value clutype)" "$(repository_get_value rootfs)"
+	success
+}	
+
+#****f* bootsr/patch_files
+#  NAME
+#    patch_files
+#  SYNOPSIS
+#    function patch_files
+#  MODIFICATION HISTORY
+#  IDEAS
+#  SOURCE
+#
+function patch_files {
+  # we patch all versions here
+  for initscript in halt network netfs; do
+	if ! grep "comoonics patch " /etc/init.d/$initscript > /dev/null; then
+		echo -n "Patching $initscript ("
+		for patchfile in $(ls -1 /opt/atix/comoonics-bootimage/patches/${initscript}*.patch | sort); do
+			echo -n $(basename $patchfile)", "
+			cd /etc/init.d/ && patch -f -r /tmp/$(basename ${patchfile}).patch.rej > /dev/null < $patchfile || \
+			echo "Failure patching $initscript with patch $patchfile" >&2
+		done
+	    echo ")"
+	fi
+  done
+}
+#************ patch_files
+
+#****f* bootsr/unpatch_files
+#  NAME
+#    unpatch_files
+#  SYNOPSIS
+#    function unpatch_files
+#  MODIFICATION HISTORY
+#  IDEAS
+#  SOURCE
+#
+function unpatch_files {
+  # we patch all versions here
+  for initscript in halt network netfs; do
+	if grep "comoonics patch " /etc/init.d/$initscript > /dev/null; then
+		echo -n "Unpatching $initscript ("
+		for patchfile in $(ls -1 /opt/atix/comoonics-bootimage/patches/${initscript}*.patch | sort -r); do
+			echo -n $(basename $patchfile)", "
+			cd /etc/init.d/ && patch -R -f -r /tmp/$(basename ${patchfile}).patch.rej > /dev/null < $patchfile || \
+			echo "Failure patching $initscript with patch $patchfile" >&2
+		done
+		echo ")"
+	fi
+  done
+}
+#************ unpatch_files
+
+#****f* bootsr/check_sharedroot
+#  NAME
+#    check_sharedroot
+#  SYNOPSIS
+#    function check_sharedroot
+#  MODIFICATION HISTORY
+#  IDEAS
+#  SOURCE
+#
+function check_sharedroot {
+  local root_fstype=$1
+  if [ "$root_fstype" = "gfs" ] || [ "$root_fstype" = "ocfs2" ] || [ "$root_fstype" = "nfs" ] || [ "$root_fstype" = "glusterfs" ]; then
+    return 1
+  else
+    return 0
+  fi
+}
+#************ check_sharedroot
 
 #include /etc/sysconfig/cluster
 
@@ -302,6 +420,23 @@ case "$action" in
 	"clean")
 	    repository_clear
 	    ;;
+    "patch_files")
+  	 	check_sharedroot $rootfs
+     	sharedroot=$?
+     	if  [ -n "$rootfs" ] && [ $sharedroot ]; then
+  	      patch_files
+      	fi
+     	;;
+	"unpatch_files")
+  	 	check_sharedroot $rootfs
+     	sharedroot=$?
+     	if  [ -n "$rootfs" ] && [ $sharedroot ]; then
+  	      unpatch_files
+      	fi
+     	;;
+	 "createxfiles")
+		createxfiles
+		;;
 	*)
 		usage
 		exit 1
