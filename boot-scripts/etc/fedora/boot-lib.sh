188ca6ca41fb122f223fb694338fc3c9ee81acf0
marc <marc>
2009-08-11 09:39:32 +0000

    Fixed bug #358 where initramfs consumed memory with growing.

--- boot-scripts/etc/fedora/boot-lib.sh
+++ boot-scripts/etc/fedora/boot-lib.sh
@@ -1,5 +1,5 @@
 #
-# $Id: boot-lib.sh,v 1.2 2009-04-14 14:47:23 marc Exp $
+# $Id: boot-lib.sh,v 1.2.4.1 2009-08-11 09:39:32 marc Exp $
 #
 # @(#)$File$
 #
@@ -41,6 +41,8 @@ function create_chroot () {
   exec_local mkdir -p $chroot_path/tmp
   exec_local chmod 755 $chroot_path
 #  exec_local mount --bind /dev $chroot_path/dev
+  exec_local mount -t tmpfs none $chroot_path/dev
+
   exec_local cp -a /dev $chroot_path/
   exec_local mount -t devpts none $chroot_path/dev/pts
   exec_local mount -t proc proc $chroot_path/proc
