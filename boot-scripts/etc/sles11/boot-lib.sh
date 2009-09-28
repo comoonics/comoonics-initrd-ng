180cfb816a10e6f3d1d5edb6d01b02732439a831
marc <marc>
2009-09-28 12:45:31 +0000

    initial revision

---
+++ boot-scripts/etc/sles11/boot-lib.sh
@@ -0,0 +1,136 @@
+#
+# $Id: boot-lib.sh,v 1.1 2009-09-28 12:45:31 marc Exp $
+#
+# @(#)$File$
+#
+# Copyright (c) 2007 ATIX AG.
+# Einsteinstrasse 10, 85716 Unterschleissheim, Germany
+# All rights reserved.
+#
+# This software is the confidential and proprietary information of ATIX
+# GmbH. ("Confidential Information").  You shall not
+# disclose such Confidential Information and shall use it only in
+# accordance with the terms of the license agreement you entered into
+# with ATIX.
+#
+#****h* comoonics-bootimage/boot-lib.sh
+#  NAME
+#    boot-lib.sh
+#    $id$
+#  DESCRIPTION
+#*******
+
+#****f* boot-lib.sh/create_chroot
+#  NAME
+#    create_chroot build a chroot environment
+#  SYNOPSIS
+#    function create_chroot($chroot_source $chroot_path) {
+#  MODIFICATION HISTORY
+#  USAGE
+#  create_chroot
+#  IDEAS
+#
+#  SOURCE
+#
+function create_chroot () {
+  chroot_source=$1
+  chroot_path=$2
+
+  exec_local cp -axf $chroot_source $chroot_path
+  exec_local rm -rf $chroot_path/var/run/*
+  exec_local mkdir -p $chroot_path/tmp
+  exec_local chmod 755 $chroot_path
+  exec_local mount -t tmpfs none $chroot_path/dev
+#  exec_local mount --bind /dev $chroot_path/dev
+  exec_local cp -a /dev $chroot_path/
+  exec_local mount -t devpts none $chroot_path/dev/pts
+  exec_local mount -t proc proc $chroot_path/proc
+  exec_local mount -t sysfs sysfs $chroot_path/sys
+}
+#************ create_chroot
+
+#****f* boot-lib.sh/sles11_detectHalt
+#  NAME
+#    sles11_detectHalt build a chroot environment
+#  SYNOPSIS
+#    function sles11_detectHalt($xkillall_procsfile, $rootfss) {
+#  MODIFICATION HISTORY
+#  USAGE
+#  sles11_detectHalt
+#  IDEAS
+#
+#  SOURCE
+#
+sles11_detectHalt() {
+    local runlevel2=$1
+    local command="halt -p"
+    [ -z "$runlevel2" ] && runlevel2=0
+    local arch
+    local opts
+    
+    . /etc/sysconfig/boot
+    . /etc/sysconfig/shutdown
+    
+    if [ $runlevel2 -eq 0 ]; then # case halt
+        arch=$(/bin/uname -m)
+        command="halt"
+        mddev=""
+        opts=""
+        #
+        # Sysvinit's shutdown is knowning about the option -P and -H to set
+        # the environment variable INIT_HALT within init for the runlevel 0.
+        # If this variable is not set we use the system default.
+        #
+        if test -z "$INIT_HALT" ; then
+            case "$HALT" in
+            [Pp]*) INIT_HALT=POWEROFF ;;
+            [Hh]*) INIT_HALT=HALT     ;;
+            *) 
+                INIT_HALT=POWEROFF
+                #
+                # Check this if valid for AMD/Intel based systems
+                #
+                case "$arch" in
+                i?86|x86_64)
+                    if test -e /proc/apm -o -e /proc/acpi -o -e /proc/sys/acpi ; then
+                        INIT_HALT=POWEROFF
+                    else
+                        INIT_HALT=HALT
+                        case "$(rc_cmdline apm)" in
+                        apm=smp-power-off|apm=power-off) INIT_HALT=POWEROFF ;;
+                        esac
+                    fi
+                    ;;
+                esac
+                ;;
+            esac
+        fi
+        #
+        # The manual page of shutdown utilizes POWEROFF whereas in
+        # the code the word POWERDOWN shows up for the option -P.
+        #
+        case "$INIT_HALT" in
+        POWEROFF|POWERDOWN)
+            opts="${opts:+$opts }-p" ;;
+        esac
+        #
+        # Sometimes wake-on-lan isn't wanted, if so stop network
+        #
+        if test "$HALT_NETWORK" = "yes" ; then
+            opts="${opts:+$opts }-i"
+        fi
+        #
+        # Sometimes it is wanted to stop also the disks
+        #
+        if test "$HALT_DISKS" = "yes" ; then
+            opts="${opts:+$opts }-h"
+        fi
+    elif [ $runlevel2 -eq 6 ]; then
+        command="reboot"
+        mddev=""
+        opts="-i"
+    fi
+    echo $command -d -f -n $opts
+}
+#************** sles11_detectHalt
+
---
+++ boot-scripts/etc/sles11/network-lib.sh
@@ -0,0 +1,147 @@
+#
+# $Id: network-lib.sh,v 1.1 2009-09-28 12:45:31 marc Exp $
+#
+# @(#)$File$
+#
+# Copyright (c) 2001 ATIX GmbH, 2007 ATIX AG.
+# Einsteinstrasse 10, 85716 Unterschleissheim, Germany
+# All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+#
+#****h* boot-scripts/etc/sles8/network-lib.sh
+#  NAME
+#    network-lib.sh
+#    $id$
+#  DESCRIPTION
+#    Libraryfunctions for network support functions for SLES8.
+#*******
+
+#****f* boot-lib.sh/sles11_ip2Config
+#  NAME
+#    sles11_ip2Config
+#  SYNOPSIS
+#    function sles11_ip2Config(ipDevice, ipAddr, ipGate, ipNetmask, ipHostname) {
+#  MODIFICATION HISTORY
+#  IDEAS
+#  SOURCE
+#
+function sles11_ip2Config() {
+  local ipDevice=$1
+  local ipAddr=$2
+
+  echo "$ipAddr" | grep "[[:digit:]][[:digit:]]*.[[:digit:]][[:digit:]]*.[[:digit:]][[:digit:]]*.[[:digit:]][[:digit:]]*" </dev/null 2>&1
+  if [ -n "$ipAddr" ]; then
+    local ipNetmask=$3
+    local ipHostname=$4
+    local ipGate=$5
+  else
+    local master=$3
+    local slave=$4
+  fi
+  local MAC=$6
+  local type=$7
+  local bridge=$8
+  local onboot=$9
+  
+  if [ "$onboot" = "yes" ]; then
+  	onboot="nfsroot"
+  fi
+
+  # reformating MAC from - to :
+  MAC=${MAC//-/:}
+
+  if [ -z "$type" ]; then type="Ethernet"; fi
+  if [ -z "$ipHostname" ]; then ipHostname="localhost.localdomain"; fi
+  if [ -z "$ipDevice" ]; then ipDevice="eth0"; fi
+
+  if [ -e ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice ]; then
+    mv -f ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice ${__prefix}/etc/sysconfig/network/ifcfg-${ipDevice}.com_back
+  fi
+
+  (echo 'NM_CONTROLLED=no' &&
+   echo 'DEVICE="'$ipDevice'"' &&
+   echo 'STARTMODE="'$onboot'"' &&
+   echo 'TYPE="'$type'"') > ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice
+
+  #[ -n "$MAC" ] && [ "$MAC" != "00:00:00:00:00:00" ] && echo "HWADDR=$MAC" >> ${__prefix}/etc/sysconfig/network-scripts/ifcfg-$ipDevice
+
+  # test for vlan config
+  if [[ "$ipDevice" =~ "[a-z]+[0-9]+\.[0-9]+" ]]; then
+	echo 'VLAN="yes"' >> ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice
+  fi
+
+  if [ -n "$ipAddr" ]; then
+    if [ "$ipAddr" = "dhcp" -o "$ipAddr" = "DHCP" -o -z "$ipAddr" ]; then
+      bootproto="dhcp"
+    else
+      bootproto="static"
+    fi
+
+    echo 'BOOTPROTO="'$bootproto'"' >> ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice
+    if [ "$bootproto" != "dhcp" ]; then
+      (echo 'IPADDR="'$ipAddr'"' &&
+      if [ -n "$ipNetmask" ]; then echo 'NETMASK="'$ipNetmask'"'; fi) >> ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice
+      if [ -n "$ipGate" ]; then
+	    echo "default $ipGate 0.0.0.0 $ipDevice" > ${__prefix}/etc/sysconfig/network/ifroute-$ipDevice
+      fi
+    fi
+  else
+     [ -n "$master" ] && echo 'MASTER="'${master}'"' >> ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice
+     [ -n "$slave" ] &&  echo 'SLAVE="'${slave}'"'   >> ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice
+     [ -n "$bridge" ] && echo 'BRIDGE="'${bridge}'"' >> ${__prefix}/etc/sysconfig/network/ifcfg-$ipDevice
+  fi
+  return 0
+}
+#************ sles11_ip2Config 
+
+#****f* boot-lib.sh/nicUp
+#  NAME
+#    nicUp
+#  SYNOPSIS
+#    function nicUp()
+#  DOCUMENTATION
+#    Powers up the network interface
+#  SOURCE
+#
+nicUp() {
+   /sbin/ifup $1
+}
+#************ ifup
+
+#################
+# $Log: network-lib.sh,v $
+# Revision 1.1  2009-09-28 12:45:31  marc
+# initial revision
+#
+# Revision 1.4  2009/04/22 11:36:45  marc
+# - upstream for sles11
+#
+# Revision 1.3  2008/10/14 10:57:07  marc
+# Enhancement #273 and dependencies implemented (flexible boot of local fs systems)
+#
+# Revision 1.2  2008/09/23 18:24:38  marc
+# fixed bug#272 where static ips would not be specified in network configuration.
+#
+# Revision 1.1  2008/08/14 13:30:52  marc
+# initial revision
+#
+# Revision 1.2  2007/12/07 16:40:00  reiner
+# Added GPL license and changed ATIX GmbH to AG.
+#
+# Revision 1.1  2006/05/07 11:33:40  marc
+# initial revision
+#
