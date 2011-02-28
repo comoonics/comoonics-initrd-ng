b1b2ce7a322efa9794d09475b4f11b1dd313bc04
marc <marc>
2011-02-28 09:00:42 +0000

    now testing all available errors.

--- tests/test-errors.sh
+++ tests/test-errors.sh
@@ -3,6 +3,8 @@ errorbin() {
 	return 1
 }
 
+path=$(dirname $0)
+
 if ! $(runonce); then
   echo -n "Testing exec_local"
   exec_local errorbin > /dev/null
@@ -19,4 +21,15 @@ if ! $(runonce); then
   test "$error1" = "$error2"
   detecterror $? "Testerrormsg for clutype $clutype returned wrong result \"$error1\" != \"$error2\"!!" || echo -n " Failed"
   echo
+  for errormsg in err_test err_cc_validate err_cc_wrongbootparamter err_clusterfs_fsck \
+                  err_clusterfs_mount err_cc_nodeid err_cc_nodename err_hw_nicdriver \
+                  err_nic_ifup err_nic_load err_nic_config err_storage_config err_storage_lvm \
+                  err_cc_setup err_rootfs_device err_rootfs_mount err_fs_mount_cdsl err_cc_restart_service \
+                  err_fs_device err_fs_mount; do
+    expectedresult=$(cat $path/test/error/$errormsg.out)
+    result=$(errormsg $errormsg param1 param2 param3 param4)
+    test $? -eq 0 && test "$(echo $result)" = "$(echo $expectedresult)"
+    detecterror $? "errormsg ${errormsg} failed. result: $result, expected: $expectedresult." || echo " Failed"
+#    echo " $result"
+  done
 fi
