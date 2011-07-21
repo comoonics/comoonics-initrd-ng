8befb753e0e01c2056443a2cd19eb8f8f9fa78bc
Marc Grimme <grimme@atix.de>
2011-07-21 15:13:37 +0200

    renamed files.

--- system-cfg-files/pre.mkinitrd.d/00-bootimage-check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VALIDFS="ext3 ext4 nfs gfs ocfs2 gfs2 glusterfs"
-
-if ! rpm -qa 'comoonics-bootimage-listfiles*' | grep "\("$(echo "$VALIDFS" | sed -e 's/[[:space:]][[:space:]]*/\\|/g')"\)" >/dev/null 2>&1; then
-  error_local "Could not find any software package of the following types. 
-Please choose from one of the listed packages and install:."
-  return 1
-fi
--- system-cfg-files/pre.mkinitrd.d/00-cdsl-check.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-MKCDSLINFRASTRUCTURE=${MKCDSLINFRASTRUCTURE:-"/usr/bin/com-mkcdslinfrastructure"}
-
-# Check if the binary exists
-if [ -e "$MKCDSLINFRASTRUCTURE" ]; then
-  $MKCDSLINFRASTRUCTURE --list >/dev/null
-fi
