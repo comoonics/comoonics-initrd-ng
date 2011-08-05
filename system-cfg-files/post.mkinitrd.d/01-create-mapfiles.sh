f63dfdfa3a2799726a47286f4d85ada23d41c95d
Marc Grimme <grimme@atix.de>
2011-08-05 09:59:51 +0200

    node ids file creation is moved to pre.mkinitrd.d/60-osr-repository-generate.sh

--- system-cfg-files/post.mkinitrd.d/01-create-mapfiles.sh
+++ system-cfg-files/post.mkinitrd.d/01-create-mapfiles.sh
@@ -8,10 +8,7 @@ fi
 if ! repository_has_key osrquerymap; then
   repository_store_value osrquerymap /etc/comoonics/querymap.cfg
 fi
-
-nodeids=$(cc_get nodeids)
-mkdir -p $(dirname ${DESTFILE}$(osr_nodeids_file)) $(dirname ${DESTFILE}/$(osr_nodeid_file 1))
-osr_create_nodeids_file $(repository_get_value clutype "") $(repository_get_value cluster_conf "") $querymap $nodeids > ${DESTFILE}$(osr_nodeids_file)
+mkdir -p $(dirname ${DESTFILE}/$(osr_nodeid_file 1))
 for nodeid in $(cc_get nodeids); do 
 	osr_generate_nodevalues $nodeid $querymap > ${DESTFILE}/$(osr_nodeid_file ${nodeid})
 	nodename=$(osr_get_nodename_by_id ${DESTFILE}/$(osr_nodeids_file) $nodeid ${DESTFILE}/$(osr_nodeid_file $nodeid))
