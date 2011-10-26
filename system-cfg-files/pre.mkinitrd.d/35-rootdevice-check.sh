10dbe61213dd6169b03d6d76a1b88628e713ed4d
Marc Grimme <grimme@atix.de>
2011-10-26 15:00:15 +0200

    Now first check /etc/fstab then /etc/mtab to find root file system.

--- system-cfg-files/pre.mkinitrd.d/35-rootdevice-check.sh
+++ system-cfg-files/pre.mkinitrd.d/35-rootdevice-check.sh
@@ -4,18 +4,31 @@ sourceLibs ${prgdir:-/opt/atix/comoonics-bootimage/boot-scripts}
 
 root=$(repository_get_value root)
 distribution=$(repository_get_value distribution)
+fstab=${fstab:-/etc/fstab}
 
 if [ -z "$root" ]; then
-	echo_local_debug -n "Trying to guess root device from currently mounted filesystems.."
-    root=$(get_filesystem / rootfs | awk '{print $1;}')  # exclude rootfs
+    echo_local_debug -n "Trying to guess root device from $fstab.."
+    root=$(awk '
+$2 == "/" {
+    print $1;
+}' $fstab)
     if [ -z "$root" ]; then
-    	error "Could not detect rootfilesystem from running system."
-    	exit 1
+        echo_local_debug -n "Trying to guess root device from currently mounted filesystems.."
+        root=$(get_filesystem / rootfs | awk '{print $1;}')  # exclude rootfs
+        if [ -z "$root" ]; then
+    	   error "Could not detect rootfilesystem from running system."
+    	   exit 1
+        fi
     fi
-    echo_local_debug " $root"
-    repository_store_value root $root
+    if [ -z "$root" ]; then
+        echo_local "Could not detect root filesystem."
+        echo_local "You must specify root= as boot parameter."
+    else
+        echo_local_debug " $root"
+        repository_store_value root $root
+    fi
     if device_mapper_multipath_check $root; then
-    	repository_store_value scsi_failover mapper
+        repository_store_value scsi_failover mapper
     else
         repository_store_value scsi_failover driver
     fi
