9ce4a67535b00555b464b82562134b74b922f7d1
Marc Grimme <grimme@atix.de>
2011-11-04 13:22:59 +0100

    initial revision.

---
+++ system-cfg-files/post.mkinitrd.d/19-copy-network-configurations.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+prgdir=${prgdir:-/opt/atix/comoonics-bootimage/boot-scripts}
+source ${prgdir}/etc/std-lib.sh
+sourceLibs ${prgdir}
+
+cdslinvadm=${cdslinvadm:-"/usr/bin/com-cdslinvadm"}
+cdsltree="/"$($cdslinvadm get tree)
+cdsllink="/"$($cdslinvadm get link)
+distro=${distro:-$(repository_get_value distribution)}
+networkconfigdir=${networkconfigdir:-$(${distro}_get_networkpath)}
+networkconfigfilter=${networkconfigfilter:-'ifcfg*'}
+CONFDIR=$(repository_get_value confdir)
+if [ -d "${networkconfigdir}" ]; then
+    [ -d ${DEST_PATH}/${CONFDIR}/network ] || mkdir -p ${DEST_PATH}/${CONFDIR}/network
+    for nicconfig in ${networkconfigdir}/${networkconfigfilter}; do
+        # If network configuration is cdsl copy all cdsl where they belong
+        if cdslinfo=$($cdslinvadm getcdsl $nicconfig 2>/dev/null); then
+            for nodeid in $(cc_get_nodeids); do 
+                if [ -f "${cdsltree}/${nodeid}/${nicconfig}" ]; then
+                    . ${cdsltree}/${nodeid}/$nicconfig
+                    if [ "${STARTMODE}" = "nfsroot" ]; then
+                        echo_local_debug "Copying $nicconfig => ${DEST_PATH}/${CONFDIR}/network/${nicconfig}.${nodeid}"
+                        cp ${cdsltree}/${nodeid}/$nicconfig ${DEST_PATH}/${CONFDIR}/network/$(basename ${nicconfig}).${nodeid} 
+                    fi
+                    unset STARTMODE
+                fi
+            done
+        else
+            . $nicconfig
+            if [ "${STARTMODE}" = "nfsroot" ]; then
+                for nodeid in $(cc_get_nodeids); do 
+                    if [ -f "${nicconfig}" ]; then
+                        echo_local_debug "Copying $nicconfig => ${DEST_PATH}/${CONFDIR}/network/${nicconfig}.${nodeid}"
+                        cp $nicconfig ${DEST_PATH}/${CONFDIR}/network/$(basename ${nicconfig}).${nodeid} 
+                    fi
+                done
+            fi
+            unset STARTMODE
+        fi
+    done
+fi
