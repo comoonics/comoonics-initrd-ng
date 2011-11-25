c41e335c033cd565ceefe3fba0118b6a24682f41
Marc Grimme <grimme@atix.de>
2011-11-25 17:15:04 +0100

    removed a error message.

--- system-cfg-files/post.mkinitrd.d/98-copy-template-repository.sh
+++ system-cfg-files/post.mkinitrd.d/98-copy-template-repository.sh
@@ -1,7 +1,7 @@
 TEMPLATE_REPOSITORY_DIR=/var/cache/comoonics-bootimage/repository
 [ -f /etc/comoonics/comoonics-bootimage.cfg ] && . /etc/comoonics/comoonics-bootimage.cfg
 
-if [ -d $TEMPLATE_REPOSITORY_DIR ]; then
+if [ -d $TEMPLATE_REPOSITORY_DIR ] && [ "$(ls -1 $TEMPLATE_REPOSITORY_DIR | wc -l)" -gt 0 ]; then
     echo_local_debug "Copying template repository from $TEMPLATE_REPOSITORY_DIR => $REPOSITORY_PATH.."
     cp $TEMPLATE_REPOSITORY_DIR/* $REPOSITORY_PATH
 fi
