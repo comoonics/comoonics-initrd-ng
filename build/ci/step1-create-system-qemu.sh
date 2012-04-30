3e72aa2a9e74665686b5a25d77688dedb7eb4d04
Marc Grimme <grimme@atix.de>
2012-04-30 17:45:28 +0200

    added pre removal

--- build/ci/step1-create-system-qemu.sh
+++ build/ci/step1-create-system-qemu.sh
@@ -2,6 +2,10 @@
 JOBNAME=${1:-"nfsclusterconf1"}
 source ${JOBNAME}/virt.sh
 source ${JOBNAME}/vm1.sh
+echo "First clean up old storage files.."
+virsh vol-delete $VM_DISK1
+echo "Ignore Error if disk does not exist."
+
 echo "First the template machine for installation has to be cloned.."
 virt-clone --connect=qemu:///system --name="$VM_NAME" --original="$VM_SOURCE_NAME" --mac="$VM_NIC1_MAC" --replace --file=$VM_DISK1 --quiet --force && \
 echo "Success" || { echo "Failed"; exit 1; }
